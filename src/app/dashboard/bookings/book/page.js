310aa1ed80a82b55ba5188193c19fa9b677a06b8
Joe <jwebpost208@gmail.com>
2026-06-01 22:43:51 +0300

    phase 44 booking page icon
    Signed-off-by: Joe <jwebpost208@gmail.com>

--- src/app/dashboard/bookings/book/page.js
+++ src/app/dashboard/bookings/book/page.js
@@ -226,11 +226,11 @@ export default function BookServicePage() {
 
           {/* Type Filter */}
           <div className="relative">
-            <Filter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={20} />
+            <Filter className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400" size={16} />
             <select
               value={selectedType}
               onChange={(e) => setSelectedType(e.target.value)}
-              className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
+              className="w-full pl-9 pr-4 py-2 border border-gray-300 rounded-lg text-sm focus:ring-2 focus:ring-blue-500"
             >
               <option value="all">All Types</option>
               {providerTypes.map(type => (
